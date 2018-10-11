0a48e4f741a390ad91a91b7807bbf54b63e22b33
Quyết Nguyễn <quyet@lingio.co>
2018-10-11 20:47:22 +0700

    Adding React . But it's not working, we need to have webpack to translate this code to browser to understand

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello from React skeleton</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
